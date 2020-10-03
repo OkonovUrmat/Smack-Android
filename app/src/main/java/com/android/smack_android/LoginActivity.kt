df1ef959e8b274f7bc9a8843e52607ec6c9dbd02
OkonovUrmat <okonov.okonov@gmail.com>
2020-10-03 17:40:18 +0600

    LoginActivity: Create button's functions

--- app/src/main/java/com/android/smack_android/LoginActivity.kt
+++ app/src/main/java/com/android/smack_android/LoginActivity.kt
@@ -2,10 +2,19 @@ package com.android.smack_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginBtnClicked(view: View) {
+
+    }
+
+    fun loginCreateUserBtnClicked(view: View) {
+
     }
 }
