22cb3ff21cd25c0882da4e66b5d1be9f47923145
OkonovUrmat <okonov.okonov@gmail.com>
2020-10-06 19:46:46 +0600

    CreateUserActivity: Create functions of buttons

--- app/src/main/java/com/android/smack_android/CreateUserActivity.kt
+++ app/src/main/java/com/android/smack_android/CreateUserActivity.kt
@@ -2,10 +2,23 @@ package com.android.smack_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateBackgroundColorClick(view: View) {
+
+    }
+
+    fun createUserClick(view: View) {
+
     }
 }
