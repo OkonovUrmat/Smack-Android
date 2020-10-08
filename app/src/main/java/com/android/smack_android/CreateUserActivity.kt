d764534f27ba2f12b2dd93a359318c2c4576e4ea
OkonovUrmat <okonov.okonov@gmail.com>
2020-10-08 21:08:26 +0600

    Setup random generated userAvatar

--- app/src/main/java/com/android/smack_android/CreateUserActivity.kt
+++ app/src/main/java/com/android/smack_android/CreateUserActivity.kt
@@ -3,15 +3,28 @@ package com.android.smack_android
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
     fun generateUserAvatar(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        userAvatar = if (color == 0) {
+            "light$avatar"
+        } else {
+            "dark$avatar"
+        }
     }
 
     fun generateBackgroundColorClick(view: View) {
