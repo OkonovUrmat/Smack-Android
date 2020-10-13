1f224e63a69f4ab9370fada98873ad1725503373
OkonovUrmat <okonov.okonov@gmail.com>
2020-10-13 20:29:38 +0600

    AuthService: Create a val jsonBody

--- app/src/main/java/com/android/smack_android/Services/AuthService.kt
+++ app/src/main/java/com/android/smack_android/Services/AuthService.kt
@@ -1,11 +1,12 @@
 package com.android.smack_android.Services
 
 import android.content.Context
+import org.json.JSONObject
 
 object AuthService {
 
     fun registerUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit) {
-        
+        val jsonBody = JSONObject()
     }
 
 }
