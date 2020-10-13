d3da8fa6e000ad193717a3cdd8941e270842bec6
OkonovUrmat <okonov.okonov@gmail.com>
2020-10-13 20:28:15 +0600

    AuthService: Create a fun registerUser

--- app/src/main/java/com/android/smack_android/Services/AuthService.kt
+++ app/src/main/java/com/android/smack_android/Services/AuthService.kt
@@ -1,4 +1,11 @@
 package com.android.smack_android.Services
 
+import android.content.Context
+
 object AuthService {
+
+    fun registerUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit) {
+        
+    }
+
 }
