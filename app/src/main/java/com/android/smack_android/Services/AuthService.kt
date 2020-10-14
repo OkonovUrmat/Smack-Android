280a2c3e67278f4cd39f117668a3db18b6386384
OkonovUrmat <okonov.okonov@gmail.com>
2020-10-14 20:25:56 +0600

    AuthService: Remove redundant space

--- app/src/main/java/com/android/smack_android/Services/AuthService.kt
+++ app/src/main/java/com/android/smack_android/Services/AuthService.kt
@@ -9,7 +9,6 @@ import com.android.volley.toolbox.Volley
 import org.json.JSONObject
 
 object AuthService {
-
     fun registerUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit) {
         val jsonBody = JSONObject()
         jsonBody.put("email", email)
