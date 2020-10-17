ed3a71da42531084fd7217951f27533964c25228
OkonovUrmat <okonov.okonov@gmail.com>
2020-10-17 18:57:44 +0600

    Constants: Create constant for login url

--- app/src/main/java/com/android/smack_android/Utilities/Constants.kt
+++ app/src/main/java/com/android/smack_android/Utilities/Constants.kt
@@ -2,3 +2,4 @@ package com.android.smack_android.Utilities
 
 const val BASE_URL = "http://localhost:3005/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
