8d3f3e718f5d6a6bcb67d667a6cbeb3408eb8d2b
OkonovUrmat <okonov.okonov@gmail.com>
2020-11-19 20:33:07 +0600

    CreateUserActivity: Set the fun errorToast private

--- app/src/main/java/com/android/smack_android/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/android/smack_android/Controller/CreateUserActivity.kt
@@ -90,7 +90,7 @@ class CreateUserActivity : AppCompatActivity() {
         }
     }
 
-    fun errorToast() {
+    private fun errorToast() {
         Toast.makeText(this, "Something wrong, please try again. ", Toast.LENGTH_LONG).show()
         enableSpinner(false)
     }
